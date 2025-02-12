93ecd64bb0b264f8b4b0a21569e9e4f1c9ab908e
Davide Mancini <mancini.davide@iisgalvanimi.edu.it>
2025-02-12 10:07:07 +0000

    logout e visualizza playlist

--- app.py
+++ app.py
@@ -4,17 +4,18 @@ from spotipy.oauth2 import SpotifyOAuth
 #le tue credenziali le trovi nella dashboard di prima
 SPOTIFY_CLIENT_ID = "0a72ccbf55f54b079a1a9674cc44e6df"
 SPOTIFY_CLIENT_SECRET = "0ec670cc57bf4cd89ba5ae0557604e1a"
-SPOTIFY_REDIRECT_URI = "https://5000-mancinidavi-progettospo-0x421oo9wik.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
+SPOTIFY_REDIRECT_URI = "https://5000-mancinidavi-progettospo-yut424jho57.ws-eu117.gitpod.io/callback" #dopo il login andiamo qui
 
 app = Flask(__name__)
 app.secret_key = 'chiave_per_session' #ci serve per identificare la sessione
 
 #config SpotifyOAuth per l'autenticazione e redirect uri
 sp_oauth = SpotifyOAuth(
-client_id=SPOTIFY_CLIENT_ID,
-client_secret=SPOTIFY_CLIENT_SECRET,
-redirect_uri=SPOTIFY_REDIRECT_URI,
-scope="user-read-private" #permessi x informazioni dell'utente
+    client_id=SPOTIFY_CLIENT_ID,
+    client_secret=SPOTIFY_CLIENT_SECRET,
+    redirect_uri=SPOTIFY_REDIRECT_URI,
+    scope="user-read-private", #permessi x informazioni dell'utente
+    show_dialog=True #forziamo la richiesta di inserire new credenziali
 )
 
 @app.route('/')
@@ -34,12 +35,19 @@ def home():
     token_info = session.get('token_info', None) #recupero token sissione (salvato prima)
     if not token_info:
         return redirect(url_for('login'))
-        
     sp = spotipy.Spotify(auth=token_info['access_token']) #usiamo il token per ottenere i dati del profilo
     user_info = sp.current_user()
     print(user_info) #capiamo la struttura di user_info per usarle nel frontend
-    return render_template('home.html', user_info=user_info) #passo le info utente all'home.html
+    playlists = sp.current_user_playlists() #sempre tramite il token sp preso prima
+    playlists_info = playlists['items'] #prendiamo solo la lista delle playlist
+    return render_template('home.html', user_info=user_info, playlists=playlists_info)
+
+@app.route('/logout')
+def logout():
+    session.clear() #cancelliamo l'access token salvato in session
+    return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
-        app.run(debug=True)
+    app.run(debug=True)
